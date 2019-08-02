45faab91858b885d7d079ac35cf84e001b156af2
dks2000dks <47571301+dks2000dks@users.noreply.github.com>
2019-08-02 19:47:17 +0530

    Update Audio.py

--- EE2350/Audio.py
+++ EE2350/Audio.py
@@ -7,9 +7,10 @@ from scipy.io.wavfile import read
 from scipy.io.wavfile import write
 
 
-rate,audio = read('Audio.wav')
-
-print (audio)
+rate,audio_original = read('Audio_Original.wav')
+audio = audio_original[:,0]
+write("Audio_modified.wav",rate,audio)
+print (audio.shape[0])
 print (audio.shape[0]/rate)								# Time of track
 # print (audio.shape[1])								# No.of Channels
 
@@ -27,41 +28,41 @@ def Add_Noise(audio,mu = 0,sigma = 1):
 	
 	return audio
 	
-def Mean_Filter(audio,M):
-	p,q,s = M,audio.shape[0]- M,audio.shape[0]- 2*M
+def Median_Filter(audio,M):
+	p,q,s = M,audio.shape[0]- M,audio.shape[0]
+	audio_change = np.zeros(s+2*M)
+	audio_change[M:s+M] = audio
 	audio_new = np.zeros(s)
 		
-	for i in range(p,q):
-		audio_new[i-p] = np.mean(audio[i-M:i+M])
+	for i in range(M,s+M):
+		audio_new[i-M] = np.median(audio_change[i-M:i+M])
 	
 	time = np.arange(s)	
-	plt.plot(time,audio_new)
-	plt.show()
+	
+	return audio_new,time
 
-		
-	return audio_new
-
-def Median_Filter(audio,M):
-	p,q,s = M,audio.shape[0]- M,audio.shape[0]- 2*M
+def Mean_Filter(audio,M):
+	p,q,s = M,audio.shape[0]- M,audio.shape[0]
+	audio_change = np.zeros(s+2*M)
+	audio_change[M:s+M] = audio
 	audio_new = np.zeros(s)
 		
-	for i in range(p,q):
-		audio_new[i-p] = np.median(audio[i-M:i+M])
+	for i in range(M,s+M):
+		audio_new[i-M] = np.mean(audio_change[i-M:i+M])
 	
-	time = np.arange(s)
-	plt.plot(time,audio_new)
-	plt.show()
+	time = np.arange(s)	
 	
-	return audio_new
-
+	return audio_new,time
 
 Plot_Audio(audio)
 audio = Add_Noise(audio)
 write("Audio_with_Noise.wav",rate,audio)
 Plot_Audio(audio)
 
-audio_new_mean = Mean_Filter(audio,100)
+audio_new_mean,time_new = Mean_Filter(audio,2)
+Plot_Audio(audio_new_mean)
 write("Audio_with_Noise_Filtered_Mean.wav",rate,audio_new_mean)
 		
-audio_new_median = Median_Filter(audio,100)
+audio_new_median,time_new = Median_Filter(audio,1)
+Plot_Audio(audio_new_median)
 write("Audio_with_Noise_Filtered_Median.wav",rate,audio_new_median)
