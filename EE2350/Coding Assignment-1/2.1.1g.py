0967344e27997f525aa2e679a42d78c73a0c899f
dks2000dks <47571301+dks2000dks@users.noreply.github.com>
2019-08-18 12:20:33 +0530

    Update 2.1.1g.py

--- EE2350/Coding Assignment-1/2.1.1g.py
+++ EE2350/Coding Assignment-1/2.1.1g.py
@@ -10,35 +10,32 @@ time = np.arange(n)
 for i in range(n):														# Generating the input
 	x[i] = 0.95 ** i
 	
-def Signal_Ideal_Delay(signal,d):										# Function to generate ideal delay in signal
+def Signal_Ideal_Delay(signal,d = 2):									# Function to generate ideal delay in signal
 	"""
 	Now we get ideal delay in signal
 	"""
-	s = signal.shape[0] - d
-	signal_delay = signal[d:]
+	s = signal.shape[0]
+	time = np.arange(+d,s+d)
 	
-	return signal_delay
+	return signal,time
 
 def Moving_Average_System(signal,M = 10):								# Function of Moving Average System using Ideal Delay System							
 	"""
-	Moving Average System using Ideal Dealy System.
+	Moving Average System using Ideal Delay System.
 	"""
 	p,q,s = M,signal.shape[0]- M,signal.shape[0]
-	signal_new = np.zeros(s)
-	add = np.zeros(s)
-		
-	for k in range(M+1):
-		add[0:s-k] = Signal_Ideal_Delay(signal,k)
-		signal_new += add
-		
-	signal_new = (signal_new)/(M + 1)
+	signal_new = np.zeros(s+M)
 	
-	time = np.arange(s)	
+	for i in range(M+1):
+		signal_new[M-i:M-i+s] += Signal_Ideal_Delay(signal,d=i)[0]
+		
+	signal_new = signal_new/(M + 1)		
+	time = np.arange(0,s+M)
 	
 	return signal_new,time
 	
 	
-x_filtered,time = Moving_Average_System(x)
+x_filtered,time_filtered = Moving_Average_System(x)
 
 plt.figure(figsize=(13, 8))
 
@@ -46,6 +43,6 @@ ax = plt.subplot(1, 2, 1)
 plt.stem(time,x,'r')
 
 ax = plt.subplot(1,2,2)
-plt.stem(time,x_filtered,'y')
+plt.stem(time_filtered,x_filtered,'y')
 
 plt.show()
